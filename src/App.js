69339e09363227bbab6e67027943d9b75ba292b8
panfuzeng <pan_fuzeng@163.com>
2019-08-02 10:53:00 +0800

    修改初始化

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Index from './pages/Home'
 class App extends React.Component {
   render() {
-    return <h1>app</h1>
+    return (
+      <div>
+        <Index />
+      </div>
+    )
   }
 }
 export default App
---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>jjjjjjjjj</h2>
+      </div>
+    )
+  }
+}
+export default Index
