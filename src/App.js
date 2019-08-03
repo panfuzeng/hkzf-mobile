1fa7647ad1fe58cb64f1cfc675b3c686ca219ace
panfuzeng <pan_fuzeng@163.com>
2019-08-03 19:45:39 +0800

    初始化项目

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import Index from './pages/Home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import City from './pages/City'
+import Map from './pages/Map'
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Index />
+        <Router>
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route path="/city" component={City} />
+            <Route path="/map" component={Map} />
+          </Switch>
+        </Router>
       </div>
     )
   }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
-class Index extends React.Component {
+import { Button } from 'antd-mobile'
+class Home extends React.Component {
   render() {
     return (
       <div>
-        <h2>jjjjjjjjj</h2>
+        <h2>这是首页列表</h2>
+        <Button type="primary">按钮</Button>
+        <Button type="primary">Primary</Button>
       </div>
     )
   }
 }
-export default Index
+export default Home
